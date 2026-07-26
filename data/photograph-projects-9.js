3969b5c5cc61c264d00d5389dc398d62072d9d94
Nogimetogo <147955879+Nogimetogo@users.noreply.github.com>
2026-07-26 15:55:02 +0900

    Add 9.0 Crown Line Photograph series

---
+++ data/photograph-projects-9.js
@@ -0,0 +1,63 @@
+window.photographProjects = window.photographProjects || [];
+
+window.photographProjects.push({
+  slug: "crown-line",
+  title: "9.0 Crown Lineは、建築の冠ではなく都市に描かれる輪郭である",
+  coverTitleLines: [
+    "9.0 Crown Line",
+    "都市に描かれる輪郭である"
+  ],
+  category: "Photograph",
+  year: "2026",
+  summary: [],
+  cover: "assets/projects/photograph/concepts/crown-line/L1000544.webp",
+  images: [
+    "assets/projects/photograph/concepts/crown-line/L1000544.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8498.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8341.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9990.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8502.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8648.webp",
+    "assets/projects/photograph/concepts/crown-line/L1001012.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8552.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9724.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8565.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9440.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF8351.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9732.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9735.webp",
+    "assets/projects/photograph/concepts/crown-line/L1000985.webp",
+    "assets/projects/photograph/concepts/crown-line/L1000986.webp",
+    "assets/projects/photograph/concepts/crown-line/L1001614.webp",
+    "assets/projects/photograph/concepts/crown-line/L1001669.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9699-r90.webp",
+    "assets/projects/photograph/concepts/crown-line/DSCF9725.webp",
+    "assets/projects/photograph/concepts/crown-line/L1000464.webp"
+  ],
+  spreads: [
+    { single: "assets/projects/photograph/concepts/crown-line/L1000544.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8498.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8341.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF9990.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8502.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8648.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/L1001012.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8552.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF9724.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8565.webp", align: "left" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF9440.webp", align: "left" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF8351.webp", align: "left" },
+    {
+      left: "assets/projects/photograph/concepts/crown-line/DSCF9732.webp",
+      right: "assets/projects/photograph/concepts/crown-line/DSCF9735.webp"
+    },
+    { single: "assets/projects/photograph/concepts/crown-line/L1000985.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/L1000986.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/L1001614.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/L1001669.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF9699-r90.webp", align: "right" },
+    { single: "assets/projects/photograph/concepts/crown-line/DSCF9725.webp", align: "center" },
+    { single: "assets/projects/photograph/concepts/crown-line/L1000464.webp", align: "center" }
+  ],
+  url: "./projects/photograph.html?slug=crown-line"
+});
